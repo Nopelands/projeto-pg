0682483c619e42986f87da693c7b76598d794346
Nopelands <rhas@cin.ufpe.br>
2021-12-26 01:41:56 -0300

    js skeleton

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
- 
+// WARNING: MATH
+// TODO interpol
+// TODO recursive casteljau funct
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var legenda = document.getElementById("Current Mode")
+
+// TODO buttons
+// TODO field
+// TODO checkboxes
+// TODO listeners
+
+var curves = [];
+
+// TODO state variables
+
+// TODO draw
